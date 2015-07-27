a01a39378f28d2b328d07e287e3503a2c36f6a4e
nekran <patricepetit@hotmail.com>
2015-07-27 22:08:26 +0200

    Redoing phpAjax tuto.

--- phpAjax/index.php
+++ phpAjax/index.php
@@ -1,25 +1,11 @@
-<?php
-
-	if(!empty($_POST) && !empty($_POST['pseudo'])){
-		session_start();
-		$_SESSION['pseudo'] = $_POST['pseudo'];
-		header('location:tchat.php');
-	}
-
-?>
 <!DOCTYPE html>
 <html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>PhP Ajax</title>
-</head>
-<body>
-	<div class="container">
-		<h1>Mon Tchat</h1>
-		<form action="index.php" method="post">
-			<input type="text" name="pseudo">
-			<input type="submit" name="envoyerPseudo" value="pseudo">
-		</form>	
-	</div>
-</body>
+  <head>
+    <meta http-equiv="content-type" content="text/html" charset="utf-8">
+    <title>
+    </title>
+  </head>
+  <body>
+
+  </body>
 </html>
